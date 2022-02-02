c314bb542eb1db2b6112c28273b8195164077c22
Pedro Fernandes <pjf_fernandes@yahoo.com.br>
2022-02-02 13:32:11 -0300

    done

--- script.js
+++ script.js
@@ -1,39 +1,47 @@
-//Functions
-const populateDisplay = ((input) => {
-  const display = document.querySelector(".display");
+/////////////////////////////////////////////////////////////Functions
+const populateDisplay = ((input, screen) => {
+  const display = document.querySelector(`.${screen}`);
   display.innerText = `${display.innerText}${input}`;
 });
 
-const getNumber = (() => {
-  const display = document.querySelector(".display");
-  return parseFloat(display.innerText);
+const getNumber = ((screen) => {
+  const display = document.querySelector(`.${screen}`);
+  return Math.abs(parseFloat(display.innerText.match(/[0-9]/)[0]));
 });
 
-const clearDisplay = (() => {
-  const display = document.querySelector(".display");
+const clearDisplay = ((screen) => {
+  const display = document.querySelector(`.${screen}`);
   display.innerText = "";
 })
 
-const getOperator = (() => {
-  const display = document.querySelector(".display");
+const getOperator = ((screen) => {
+  const display = document.querySelector(`.${screen}`);
   return display.innerText[0];
 });
 
-//Clear button event
+const getDisplay = ((screen) => {
+  const display = document.querySelector(`.${screen}`);
+  return display.innerText;
+});
+/////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////Clear button event
 const clear = document.querySelector(".clear");
 clear.addEventListener("click", () => {
-  clearDisplay();
+  clearDisplay("last");
+  clearDisplay("next");
+  numbers = [];
+  operator = [];
 });
-
-//Operations
+/////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////Operations
 let numbers = [];
-let operator;
+let operator = [];
 
 const keys = document.querySelectorAll(".num");
 keys.forEach((element) => {
 
   element.addEventListener("click", (event) => {
-    populateDisplay(event.target.innerText);
+    populateDisplay(event.target.innerText, "next");
   });
 
 });
@@ -41,16 +49,61 @@ keys.forEach((element) => {
 const operations = document.querySelectorAll(".op")
 operations.forEach((element) => {
   element.addEventListener("click", (event) => {
-    numbers.push(getNumber());
-    clearDisplay();
-    populateDisplay(event.target.innerText);
+    numbers.push(getNumber("next"));
+    populateDisplay(getDisplay("next"), "last");
+    clearDisplay("next");
+    populateDisplay(event.target.innerText, "next");
+    operator.push(getOperator("next"));
+    console.log(operator);
+    console.log(numbers);
+
   });
 });
 
 const equals = document.querySelector(".equals")
 
 equals.addEventListener("click", (event) => {
-  numbers.push(getNumber());
-  operator = getOperator();
+  numbers.push(getNumber("next"));
+  operator.push(getOperator("next"));
+  populateDisplay(getDisplay("next"), "last")
+
+
+  clearDisplay("next");
+
   console.log(operator);
+  console.log(numbers);
+
+
 });
+
+/////////////////////////////////////////////////Operations functions
+function add(x, y) {
+  return x+y;
+}
+
+function subtract(x, y) {
+  return x-y;
+}
+
+function multiply(x, y) {
+  return x*y;
+}
+
+function divide(x, y) {
+  return x/y;
+}
+
+function operate(x, y, op) {
+
+  if (op === "+") {
+      return add(x, y);
+  } else if (op === "-") {
+      return subtract(x, y);
+  } else if (op === "x") {
+      return multiply(x, y);
+  } else if (op === "/") {
+      return divide(x, y);
+  }
+
+}
+////////////////////////////////////////////////////////////////////
