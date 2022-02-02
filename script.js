9c94b29108e06ab5bdf154ce027d8b15fecda734
Pedro Fernandes <pjf_fernandes@yahoo.com.br>
2022-02-02 00:05:21 -0300

    done

--- script.js
+++ script.js
@@ -1,15 +1,51 @@
-let lastInput;
-let newInput;
-
+//Functions
 const populateDisplay = ((input) => {
   const display = document.querySelector(".display");
   display.innerText = `${display.innerText}${input}`;
 });
 
-const keys = document.querySelectorAll(".key");
+const getNumber = (() => {
+  const display = document.querySelector(".display");
+  return parseFloat(display.innerText);
+});
+
+const clearDisplay = (() => {
+  const display = document.querySelector(".display");
+  display.innerText = "";
+})
+
+//Clear button event
+const clear = document.querySelector(".clear");
+clear.addEventListener("click", () => {
+  clearDisplay();
+});
+
+//Operations
+let numbers = [];
+
+const keys = document.querySelectorAll(".num");
 keys.forEach((element) => {
+
   element.addEventListener("click", (event) => {
-    const key = element;
-    populateDisplay(key.id);
+    populateDisplay(event.target.innerText);
   });
+
+});
+
+const operations = document.querySelectorAll(".op")
+operations.forEach((element) => {
+  element.addEventListener("click", (event) => {
+    numbers.push(getNumber());
+    clearDisplay();
+    populateDisplay(event.target.innerText);
+    numbers.push()
+    console.log(numbers);
+  });
+});
+
+const equals = document.querySelector(".equals")
+
+equals.addEventListener("click", (event) => {
+  numbers.push(getNumber());
+  console.log(numbers);
 });
